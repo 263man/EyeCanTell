4b862386726fa2905d032ef9233220b4ca074b78
263man <48086019+263man@users.noreply.github.com>
2021-11-16 19:44:46 +0200

    Update weights.py

--- architectures/weights.py
+++ architectures/weights.py
@@ -4,5 +4,6 @@ Video Face Manipulation Detection Through Ensemble of CNNs
 """
 
 weight_url = {
-'EfficientNetAutoAttB4_DFDC':'https://github.com/263man/EyeCanTell/blob/master/architectures/nvidia_efficientnet-b4_210412.pth'
+'EfficientNetAutoAttB4_DFDC':'https://github.com/263man/EyeCanTell/blob/master/architectures/nvidia_efficientnet-b4_210412.pth',
+'EfficientNetAutoAttB4ST_DFDC':'https://github.com/263man/EyeCanTell/blob/master/architectures/nvidia_efficientnet-b4_210412.pth',
 }
